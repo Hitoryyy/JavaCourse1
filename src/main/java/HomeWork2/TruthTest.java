9c5382f4f11be0e13103b3b8e243ba4751fa5ad4
Hitory <hitory@yahoo.com>
2022-02-15 11:04:53 +0300

    Домашняя работа № 2

---
+++ src/main/java/HomeWork2/TruthTest.java
@@ -0,0 +1,12 @@
+package HomeWork2;
+
+public class TruthTest {
+    public static boolean truthTest(int x, int y) {
+        int result = x + y;
+        if (result >= 10 && result <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/HomeWork2/TruthTestNum.java
@@ -0,0 +1,11 @@
+package HomeWork2;
+
+public class TruthTestNum {
+    public static boolean truthTestnum(int b) {
+        if (b < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
